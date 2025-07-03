f684ac9ca56d619402e5094a79c22ee339398173
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-07-03 20:52:52 -0300

    refactor: corrigir definição do modelo CT removendo telefone e ajustando proprietario

--- src/api_atletas/Centro_de_treinamento/schemas.py
+++ src/api_atletas/Centro_de_treinamento/schemas.py
@@ -7,5 +7,5 @@ class CT(BaseSchema):
     id: Annotated[int, Field(ge=1, description="ID do centro de treinamento")]
     nome: Annotated[str, Field(min_length=3, max_length=50, description="Nome do centro de treinamento")]
     endereco: Annotated[str, Field(min_length=3, max_length=100, description="Endereço do centro de treinamento")]
-    telefone: Annotated[str, Field(min_length=10, max_length=15, description="Telefone do centro de treinamento")]
+    proprietario: Annotated[str, Field(min_length=3, max_length=50, description="Nome do proprietário do centro de treinamento")]
     
